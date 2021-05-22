0d9f8bdb235c1a81462003652beb85ed30fac29f
pierun <84330061+pierun@users.noreply.github.com>
2021-05-22 11:56:56 +0200

    Add files via upload

---
+++ lab2Zad1BartłomiejKierońskiW62951/Zad1/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace Zad1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.plecM = new System.Windows.Forms.RadioButton();
+            this.plecK = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(86, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Oblicz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(13, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Podaj wzrost [cm]";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(343, 75);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(117, 94);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Waga";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 23);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(95, 19);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Standardowa";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(8, 66);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(64, 19);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Idealna";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // plecM
+            // 
+            this.plecM.AutoSize = true;
+            this.plecM.Location = new System.Drawing.Point(101, 149);
+            this.plecM.Name = "plecM";
+            this.plecM.Size = new System.Drawing.Size(82, 19);
+            this.plecM.TabIndex = 3;
+            this.plecM.TabStop = true;
+            this.plecM.Text = "Mezczyzna";
+            this.plecM.UseVisualStyleBackColor = true;
+            // 
+            // plecK
+            // 
+            this.plecK.AutoSize = true;
+            this.plecK.Location = new System.Drawing.Point(220, 149);
+            this.plecK.Name = "plecK";
+            this.plecK.Size = new System.Drawing.Size(65, 19);
+            this.plecK.TabIndex = 4;
+            this.plecK.TabStop = true;
+            this.plecK.Text = "Kobieta";
+            this.plecK.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(190, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(14, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Podaj Płeć";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 345);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.plecK);
+            this.Controls.Add(this.plecM);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RadioButton plecM;
+        private System.Windows.Forms.RadioButton plecK;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ lab2Zad1BartłomiejKierońskiW62951/Zad1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zad1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (plecM.Checked && checkBox1.Checked && checkBox2.Checked==false || plecK.Checked && checkBox1.Checked && checkBox2.Checked == false) 
+            {
+                float x;
+                float y = 100;
+                x = float.Parse(textBox1.Text);
+                x = x - y ;
+
+                label3.Text = "Standardowa waga to " + x+"kg";
+            }
+            else if (plecM.Checked && checkBox2.Checked && checkBox1.Checked == false)
+            {
+                float x;
+                int y = 100;
+                const double V = 0.9;
+                float z = (float)V;
+                float v = float.Parse(textBox1.Text);
+                x = v;
+                x = x - y;
+                x= x* z;
+                label3.Text = "Idealna waga to " + x + "kg";
+            }
+            else if (plecK.Checked && checkBox2.Checked && checkBox1.Checked == false)
+            {
+                float x;
+                int y = 100;
+                const double V = 0.85;
+                float z = (float)V;
+                x = float.Parse(textBox1.Text);
+                x = x - y;
+                x = x * z;
+                label3.Text = "Idealna waga to " + x + "kg";
+            }
+            else if (plecM.Checked && checkBox1.Checked && checkBox2.Checked)
+            {
+                float x,k;
+                int y = 100;
+                const double V = 0.9;
+                float z = (float)V;
+                x = float.Parse(textBox1.Text);
+                x = x - y;
+                k = x;
+                x = x * z;
+                label3.Text = "Standardowa waga mężczyzny to: " + k + "kg " + "idealna waga mężczyzny to" +x+"kg";
+            }
+            else if (plecK.Checked && checkBox1.Checked && checkBox2.Checked)
+            {
+                float x, k;
+                int y = 100;
+                const double V = 0.85;
+                float z = (float)V;
+                x = float.Parse(textBox1.Text);
+                x = x - y;
+                k = x;
+                x = x * z;
+                label3.Text = "Standardowa waga kobiety to: " + k + "kg " + "idealna waga kobiety to" + x + "kg";
+            }
+            else
+            {
+                label3.Text = "Błędne dane";
+            }
+        }
+    }
+}
